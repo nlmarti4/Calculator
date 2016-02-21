43f5b9d81120b4b6cfb01a2c6633ef6f38d64f19
nlmarti4 <nlmarti4@asu.edu>
2016-02-21 16:29:51 -0700

    Initial check-in.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,76 @@
+package cse360assign3;
+
+/** This class contains a integer total
+ * methods which operate on total and
+ * display the history of total. 
+ * 
+ * 	@author Nicholas Martinez PIN 601
+ *  @version 2/21/16
+ */
+public class Calculator {
+
+	private int total;
+	
+	/** Initializes value of total
+	 * 
+	 * @param None
+	 * @return None
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Returns value of total
+	 * 
+	 * @param None
+	 * @return total	Value of total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Adds value to total
+	 * 
+	 * @param value	Value to be added to total
+	 * @return None
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtracts value from total
+	 * 
+	 * @param value	Value to be subtracted from total
+	 * @return None
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiplies total by value
+	 * 
+	 * @param value	Value to be multiply by
+	 * @return None
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Divides total by value
+	 * 
+	 * @param value	Value to be divide by
+	 * @return None
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** Returns string that represents history of total
+	 * 
+	 * @param None
+	 * @return string	String that represents history of total
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
